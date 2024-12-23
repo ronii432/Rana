ba5772a277fac303d6697844bc1262627997c745
ronii432 <rana89548051@gmail.com>
2024-12-23 11:36:11 +0530

    Delete Build.sh

--- Build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Compile the C file into a binary named 'rohit'
-gcc -o rohit rohit.c lpthread
-
-# Give execution permissions to the binary
-chmod +x rohit
-
-echo "Build process completed. Binary 'rohit' is ready."
