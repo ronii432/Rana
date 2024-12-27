9dae9fc795719bd05245e10399c669dde5c77df3
ronii432 <192245640+ronii432@users.noreply.github.com>
2024-12-27 19:38:51 +0530

    Update rohit.py

--- rohit.py
+++ rohit.py
@@ -359,17 +359,6 @@ def start_message(message):
     except Exception as e:
         print(f"Error while processing /start command: {e}")
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, Flask is running!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
 
 
 
